726b07208f0957c34c3d9acce264981c6ebc43ec
Nasir-afridi <nasirwc05@gmail.com>
2024-11-09 17:06:53 +0500

    app setup inprogress

---
+++ src/components/nav/TopNav.jsx
@@ -0,0 +1,13 @@
+import "./App.scss";
+
+const TopNav = () => {
+  return (
+    <>
+      <div className="icon">
+        <img src="./public/Search-svg.svg" className="search-icon" alt="" />
+      </div>
+    </>
+  );
+};
+
+export default TopNav;
